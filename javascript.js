80a0222bd505b8ebd87e0b5aa55d5b66dc097f4d
Qui Dang <quidang987@hotmail.com>
2023-11-29 21:51:04 +1100

    Finish function for choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors']; // Creating an array with selections
+    const selection = Math.floor(Math.random() * choices.length); //Randomizes the index in choices
+    return choices[selection]; 
+}
+
+const computerSelection = getComputerChoice();
+console.log('Computer Selects:', computerSelection);
+
+
